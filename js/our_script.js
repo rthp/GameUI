19c2822e83ca73d5b3c1e2a0bc4999df2eaa0ee9
Ranjana Thapaliya <ranjana.thapaliya@gmail.com>
2015-06-08 12:24:10 -0700

    New Added Files of Project War UI

---
+++ js/our_script.js
@@ -0,0 +1,8 @@
+// <!-- This is script for Angular -->
+
+//This is module
+var warApp = angular.module('warApp',[]);
+
+warApp.controller('WarController',function($scope) {
+	$scope.nickname = ""
+})
